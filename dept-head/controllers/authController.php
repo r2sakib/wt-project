7cb9aa99fd04e4b2b1ac93652906a079496103f6
FahimInit <fm01793176380@gmail.com>
2026-05-18 02:35:52 +0600

    Controller: Database connected for the authenticatio

--- dept-head/controllers/authController.php
+++ dept-head/controllers/authController.php
@@ -1,29 +1,40 @@
 <?php
 session_start();
 
+require_once '../config/Database.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
-    $email = htmlspecialchars(trim($_POST['loginEmailInput']));
+    $email = trim($_POST['loginEmailInput']);
     $password = trim($_POST['loginPassInput']);
 
-    $valid_email = "fahim@aiub.edu";
-    $valid_password = "admin";
+    if (isset($conn)) {
+        
+        $query = "SELECT id, name, password_hash, role FROM users WHERE email = ? AND is_active = TRUE LIMIT 1";
+        $stmt = $conn->prepare($query);
+        
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        
+        $result = $stmt->get_result();
 
-    if ($email === $valid_email && $password === $valid_password) {
-        $_SESSION['user_id'] = 1;
-        $_SESSION['name'] = "Md. Fahim Muhtashim";
-        $_SESSION['role'] = "Head of Department";
-        header("Location: ../index.php");
-        exit();
-    } else {
+        if ($result->num_rows === 1) {
+            $user = $result->fetch_assoc();
+
+            if (password_verify($password, $user['password_hash'])) {
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['name'] = $user['name'];
+                $_SESSION['role'] = $user['role'];
+                
+                header("Location: ../index.php");
+                exit();
+            }
+        }
+
         $_SESSION['login_error'] = "Invalid institutional email or password.";
         header("Location: ../index.php");
         exit();
-    }
-}
 
-if (isset($_GET['action']) && $_GET['action'] === 'logout') {
-    session_destroy();
-    header("Location: ../index.php");
-    exit();
+        $stmt->close();
+    }
 }
